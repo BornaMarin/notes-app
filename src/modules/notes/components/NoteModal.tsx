58905da82eca6f3ae22c94bd33772c7ac620175a
Fyroman <djamic.dominik@gmail.com>
2021-09-03 15:38:37 +0200

    feat(Notes): creating, updating and deleting

--- src/modules/notes/components/NoteModal.tsx
+++ src/modules/notes/components/NoteModal.tsx
@@ -1,4 +1,7 @@
-import {Fragment, useEffect} from "react";
+import {Fragment, useCallback, useEffect, useState} from "react";
+
+// Types
+import {Note} from "../types/Note";
 
 // Styles
 import './NoteModal.css'
@@ -14,17 +17,39 @@ import ReactMarkdown from "react-markdown";
 interface Props {
     isShown: boolean
     onHide(): void
-    note: string
+    note: Note | null
+    shouldOpenInEditMode: boolean
+    onSaveNote(newNote: Note): void
+    onDeleteNote(targetNote: Note): void
 }
 
 // Main
-function NoteModal({isShown, onHide, note}: Props) {
+function NoteModal({isShown, onHide, note, shouldOpenInEditMode, onSaveNote, onDeleteNote}: Props) {
+
+    const [isInEditMode, setIsInEditMode] = useState(shouldOpenInEditMode)
+    const [localContent, setLocalContent] = useState('')
 
     useEffect(() => {
+        setLocalContent(note?.content || '')
+        setIsInEditMode(shouldOpenInEditMode)
+
         const CLASS_NAME = 'overflow-hidden'
         const action: keyof DOMTokenList = isShown ? 'add' : 'remove'
         document.body.classList[action](CLASS_NAME)
-    }, [isShown])
+    }, [isShown, note, shouldOpenInEditMode])
+
+    const saveNote = useCallback(() => {
+        setIsInEditMode(false)
+        if (!note) {
+            throw new Error('Tried saving a note that is not defined.')
+        }
+
+        onSaveNote({...note, content: localContent})
+    }, [note, localContent, onSaveNote])
+
+    const setContent = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setLocalContent(event.currentTarget.value)
+    }
 
     return isShown ? (
         <Fragment>
@@ -33,12 +58,19 @@ function NoteModal({isShown, onHide, note}: Props) {
                 <div className={'notes-modal-header'}>
                     <ArrowBack onClick={onHide}/>
                     <div className={'spacer'}/>
-                    <Edit/>
-                    <Save/>
-                    <Delete/>
+                    {/* TODO: hide instead of conditional rendering */}
+                    {isInEditMode || <Edit onClick={() => setIsInEditMode(true)}/>}
+                    {isInEditMode && <Save onClick={() => saveNote()}/>}
+                    <Delete onClick={() => onDeleteNote(note!)}/>
                 </div>
                 <div className="notes-modal-content">
-                    <ReactMarkdown children={note}/>
+                    {isInEditMode ? (
+                        <textarea
+                            className={'notes-modal-editor'}
+                            value={localContent}
+                            onChange={setContent}
+                        />
+                    ) : <ReactMarkdown className={'markdown'} children={localContent}/>}
                 </div>
             </div>
         </Fragment>
--- src/modules/notes/views/NotesView.tsx
+++ src/modules/notes/views/NotesView.tsx
@@ -1,4 +1,7 @@
-import {useState} from "react";
+import {useCallback, useState} from "react";
+
+// Types
+import {Note} from "../types/Note";
 
 // Style
 import './NotesView.css'
@@ -10,32 +13,71 @@ import NoteModal from "../components/NoteModal";
 // Main
 function NotesView() {
 
-    const defaultNotes = Array.from(Array(100))
-    const [notes, setNotes] = useState(defaultNotes)
-    const [isModalShown, setIsModalShown] = useState(false)
-    const markdown = 'This is a note\n' +
-        '==============\n' +
-        '\n' +
-        'Subtitle\n' +
-        '--------\n' +
-        '\n' +
-        '\n' +
-        'Shopping list:\n' +
-        '* apples\n' +
-        '* oranges\n' +
-        '* toilet paper\n'
+    const [notes, setNotes] = useState<Note[]>([])
+    const [openedNote, setOpenedNote] = useState<Note | null>(null)
+    const [shouldOpenInEditMode, setShouldOpenInEditMode] = useState(false)
+
+    const createNewNote = useCallback(() => {
+        const DEFAULT_CONTENT = 'This is a note\n' +
+            '==============\n' +
+            '\n' +
+            'Subtitle\n' +
+            '--------\n' +
+            '\n' +
+            '\n' +
+            'Shopping list:\n' +
+            '* apples\n' +
+            '* oranges\n' +
+            '* toilet paper\n'
+
+        const LAST_ID = Math.max(...notes.map(note => note.id), 1)
+        const newNote = {
+            id: LAST_ID + 1,
+            content: DEFAULT_CONTENT
+        }
+        setNotes([...notes, newNote])
+        setOpenedNote(newNote)
+        setShouldOpenInEditMode(true)
+    }, [notes])
+
+    const openNote = useCallback((note: Note) => {
+        setOpenedNote(note)
+        setShouldOpenInEditMode(false)
+    }, [])
+
+    const updateNote = useCallback((updatedNote: Note) => {
+        const NOTE_INDEX = notes.findIndex(note => note.id === updatedNote.id)
+        const newNotes = [...notes]
+        newNotes[NOTE_INDEX] = updatedNote
+        setNotes(newNotes)
+    }, [notes])
+
+    const deleteNote = useCallback((targetNote: Note) => {
+        const NOTE_INDEX = notes.findIndex(note => note.id === targetNote.id)
+        const newNotes = [...notes]
+        newNotes.splice(NOTE_INDEX, 1)
+        setNotes(newNotes)
+        setOpenedNote(null)
+    }, [notes])
 
     return (<div className={'notes-container'}>
-        <div className={'notes-add-button'} onClick={() => setIsModalShown(true)}>+</div>
+        <div className={'notes-add-button'} onClick={() => createNewNote()}>+</div>
         {notes.map(note => (
-            <div key={note} className={'notes-item'}>
+            <div key={note.id} className={'notes-item'} onClick={() => openNote(note)}>
                 <ReactMarkdown
-                    children={markdown}
+                    children={note.content}
                     className={'notes-item-content'}
                 />
             </div>
         ))}
-        <NoteModal isShown={isModalShown} onHide={() => setIsModalShown(false)} note={markdown}/>
+        <NoteModal
+            isShown={openedNote !== null}
+            onHide={() => setOpenedNote(null)}
+            note={openedNote}
+            shouldOpenInEditMode={shouldOpenInEditMode}
+            onSaveNote={updateNote}
+            onDeleteNote={deleteNote}
+        />
     </div>)
 }
 
