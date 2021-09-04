175c57e6caa5a713cb1075207c29bba7ded691c0
Fyroman <djamic.dominik@gmail.com>
2021-09-04 21:40:02 +0200

    refactor(Notes): handling notes data moved to NotesProvider

---
+++ src/modules/notes/components/NotesProvider.tsx
@@ -0,0 +1,84 @@
+import {createContext, FC, useCallback, useEffect, useState} from "react";
+
+// Types
+import {Note} from "../types/Note";
+interface Context {
+    notes: Note[]
+    createNote(content: string): Promise<Note>
+    updateNote(id: number, content: string): void
+    deleteNote(id: number): void
+}
+
+// Context
+export const NotesContext = createContext<Context | null>(null);
+
+// Main
+const NotesProvider: FC = ({children}) => {
+
+    const [notes, setNotes] = useState<Note[]>([])
+    const NOTES_STORAGE_KEY = 'notes'
+
+    useEffect(() => {
+        const notesStorage = localStorage.getItem(NOTES_STORAGE_KEY)
+        notesStorage && setNotes(JSON.parse(notesStorage))
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem(NOTES_STORAGE_KEY, JSON.stringify(notes))
+    }, [notes])
+
+    const findNoteIndex = useCallback((id: number, sourceNotes: Note[]) => {
+        const NOTE_INDEX = sourceNotes.findIndex(note => note.id === id)
+        if (NOTE_INDEX === -1) throw new Error(`Note with ID ${id} not found!`)
+        return NOTE_INDEX
+    }, [])
+
+
+    const createNote = useCallback((content = '') => {
+        return new Promise<Note>((resolve) => {
+            setNotes((oldNotes) => {
+                const noteIds = oldNotes.map(note => note.id)
+                const LAST_ID = Math.max(...noteIds, 1)
+                const newNote = {
+                    id: LAST_ID + 1,
+                    content
+                }
+                resolve(newNote) // loš tajming :)
+                return [...oldNotes, newNote]
+            })
+        })
+    }, [])
+
+    const updateNote = useCallback((id: number, content = '') => {
+        setNotes((oldNotes) => {
+            const NOTE_INDEX = findNoteIndex(id, oldNotes)
+            const notesCopy = [...oldNotes]
+            notesCopy[NOTE_INDEX] = {id, content}
+            return notesCopy
+        })
+    }, [findNoteIndex])
+
+    const deleteNote = useCallback((id: number) => {
+        setNotes((oldNotes) => {
+            const NOTE_INDEX = findNoteIndex(id, oldNotes)
+            const notesCopy = [...oldNotes]
+            notesCopy.splice(NOTE_INDEX, 1)
+            return notesCopy
+        })
+    }, [findNoteIndex])
+
+    const value = {
+        notes,
+        createNote,
+        updateNote,
+        deleteNote
+    }
+
+    return (
+        <NotesContext.Provider value={value}>
+            {children}
+        </NotesContext.Provider>
+    )
+}
+
+export default NotesProvider
--- src/modules/notes/views/NotesView.tsx
+++ src/modules/notes/views/NotesView.tsx
@@ -1,4 +1,4 @@
-import {useCallback, useEffect, useState} from "react";
+import {useCallback, useContext, useState} from "react";
 
 // Types
 import {Note} from "../types/Note";
@@ -10,25 +10,19 @@ import './NotesView.css'
 import ReactMarkdown from 'react-markdown'
 import NoteModal from "../components/NoteModal";
 
+// Context
+import {NotesContext} from "../components/NotesProvider";
+
 // Main
 function NotesView() {
 
-    const [notes, setNotes] = useState<Note[]>([])
+    const notesContext = useContext(NotesContext)
+    if (!notesContext) throw new Error('NotesView should be nested inside NotesProvider!')
+
     const [openedNote, setOpenedNote] = useState<Note | null>(null)
     const [shouldOpenInEditMode, setShouldOpenInEditMode] = useState(false)
 
-    const NOTES_STORAGE_KEY = 'notes'
-
-    useEffect(() => {
-        const savedNotes = JSON.parse(localStorage.getItem(NOTES_STORAGE_KEY) || '[]')
-        setNotes(savedNotes)
-    }, [])
-
-    useEffect(() => {
-        localStorage.setItem(NOTES_STORAGE_KEY, JSON.stringify(notes))
-    }, [notes])
-
-    const createNewNote = useCallback(() => {
+    const createNewNote = useCallback(async () => {
         const DEFAULT_CONTENT = 'This is a note\n' +
             '==============\n' +
             '\n' +
@@ -40,56 +34,46 @@ function NotesView() {
             '* apples\n' +
             '* oranges\n' +
             '* toilet paper\n'
-
-        const LAST_ID = Math.max(...notes.map(note => note.id), 1)
-        const newNote = {
-            id: LAST_ID + 1,
-            content: DEFAULT_CONTENT
-        }
-        setNotes([...notes, newNote])
+        const newNote = await notesContext.createNote(DEFAULT_CONTENT)
         setOpenedNote(newNote)
         setShouldOpenInEditMode(true)
-    }, [notes])
+    }, [notesContext])
 
     const openNote = useCallback((note: Note) => {
         setOpenedNote(note)
         setShouldOpenInEditMode(false)
     }, [])
 
-    const updateNote = useCallback((updatedNote: Note) => {
-        const NOTE_INDEX = notes.findIndex(note => note.id === updatedNote.id)
-        const newNotes = [...notes]
-        newNotes[NOTE_INDEX] = updatedNote
-        setNotes(newNotes)
-    }, [notes])
+    const updateNote = useCallback(({id, content}: Note) => {
+        notesContext.updateNote(id, content)
+    }, [notesContext])
 
-    const deleteNote = useCallback((targetNote: Note) => {
-        const NOTE_INDEX = notes.findIndex(note => note.id === targetNote.id)
-        const newNotes = [...notes]
-        newNotes.splice(NOTE_INDEX, 1)
-        setNotes(newNotes)
+    const deleteNote = useCallback(({id}: Note) => {
+        notesContext.deleteNote(id)
         setOpenedNote(null)
-    }, [notes])
+    }, [notesContext])
 
-    return (<div className={'notes-container'}>
-        <div className={'notes-add-button'} onClick={() => createNewNote()}>+</div>
-        {notes.map(note => (
-            <div key={note.id} className={'notes-item'} onClick={() => openNote(note)}>
-                <ReactMarkdown
-                    children={note.content}
-                    className={'notes-item-content'}
-                />
-            </div>
-        ))}
-        <NoteModal
-            isShown={openedNote !== null}
-            onHide={() => setOpenedNote(null)}
-            note={openedNote}
-            shouldOpenInEditMode={shouldOpenInEditMode}
-            onSaveNote={updateNote}
-            onDeleteNote={deleteNote}
-        />
-    </div>)
+    return (
+        <div className={'notes-container'}>
+            <div className={'notes-add-button'} onClick={() => createNewNote()}>+</div>
+            {notesContext.notes.map(note => (
+                <div key={note.id} className={'notes-item'} onClick={() => openNote(note)}>
+                    <ReactMarkdown
+                        children={note.content}
+                        className={'notes-item-content'}
+                    />
+                </div>
+            ))}
+            <NoteModal
+                isShown={openedNote !== null}
+                onHide={() => setOpenedNote(null)}
+                note={openedNote}
+                shouldOpenInEditMode={shouldOpenInEditMode}
+                onSaveNote={updateNote}
+                onDeleteNote={deleteNote}
+            />
+        </div>
+    )
 }
 
 export default NotesView
