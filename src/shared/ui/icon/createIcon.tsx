f9110de1abefdf1998d61862f6423f14c0057735
Fyroman <djamic.dominik@gmail.com>
2021-09-05 01:28:17 +0200

    refactor(Icon): factory function for creating icon components

---
+++ src/shared/ui/icon/createIcon.tsx
@@ -0,0 +1,18 @@
+import {IconPassProps} from "./IconPassProps";
+import Icon from "./Icon";
+
+interface IconOptions {
+    path: string;
+    width: number;
+    height: number
+}
+
+function createIcon({path, width, height}: IconOptions) {
+    return function ({onClick}: IconPassProps) {
+        return (
+            <Icon path={path} width={width} height={height} onClick={onClick}/>
+        )
+    }
+}
+
+export {createIcon}
